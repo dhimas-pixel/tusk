1ca61d0cfa18cedfcc7491e683bb45c4e00c3ffc
Dhimas_Mbuh <ephanzen4@gmail.com>
2024-06-11 11:36:46 +0700

    Create API Login, Register, Delete, Employee Account

---
+++ controllers/user_controller.go
@@ -0,0 +1,156 @@
+package controllers
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"net/http"
+	"tusk/models"
+)
+
+type UserController struct {
+	DB *gorm.DB
+}
+
+func (u *UserController) Login(c *gin.Context) {
+
+	user := models.User{}
+
+	errBindJson := c.ShouldBindJSON(&user)
+	if errBindJson != nil {
+		c.JSON(http.StatusInternalServerError, models.Response{
+			StatusCode: http.StatusInternalServerError,
+			Message:    "Something went wrong",
+			Data:       nil,
+		})
+		fmt.Println("Error: errBindJson", errBindJson.Error())
+
+		return
+	}
+
+	password := user.Password
+	errDB := u.DB.Where("email = ?", user.Email).Take(&user).Error
+	if errDB != nil {
+		c.JSON(http.StatusUnauthorized, models.Response{
+			StatusCode: http.StatusUnauthorized,
+			Message:    "Email or Password is incorrect",
+			Data:       nil,
+		})
+		return
+	}
+
+	errHash := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if errHash != nil {
+		c.JSON(http.StatusUnauthorized, models.Response{
+			StatusCode: http.StatusUnauthorized,
+			Message:    "Email or Password is incorrect",
+			Data:       nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, models.Response{
+		StatusCode: http.StatusOK,
+		Message:    "Login success",
+		Data: gin.H{
+			"role":  user.Role,
+			"name":  user.Name,
+			"email": user.Email,
+		},
+	})
+}
+
+func (u *UserController) CreateAccount(c *gin.Context) {
+
+	user := models.User{}
+
+	errBindJson := c.ShouldBindJSON(&user)
+	if errBindJson != nil {
+		c.JSON(http.StatusInternalServerError, models.Response{
+			StatusCode: http.StatusInternalServerError,
+			Message:    "Something went wrong",
+			Data:       nil,
+		})
+		fmt.Println("Error: errBindJson", errBindJson.Error())
+		return
+	}
+
+	emailExist := u.DB.Where("email = ?", user.Email).First(&user).RowsAffected != 0
+	if emailExist {
+		c.JSON(http.StatusBadRequest, models.Response{
+			StatusCode: http.StatusBadRequest,
+			Message:    "Email already exist",
+			Data:       nil,
+		})
+		return
+	}
+
+	hashedPasswordBytes, _ := bcrypt.GenerateFromPassword([]byte("123456"), bcrypt.DefaultCost)
+
+	user.Password = string(hashedPasswordBytes)
+	user.Role = "Employee"
+
+	errDB := u.DB.Create(&user).Error
+	if errDB != nil {
+		c.JSON(http.StatusInternalServerError, models.Response{
+			StatusCode: http.StatusInternalServerError,
+			Message:    "Something went wrong",
+			Data:       nil,
+		})
+		fmt.Println("Error: errDB", errDB.Error())
+		return
+	}
+
+	c.JSON(http.StatusCreated, models.Response{
+		StatusCode: http.StatusCreated,
+		Message:    "Account created successfully",
+		Data: gin.H{
+			"role":  user.Role,
+			"name":  user.Name,
+			"email": user.Email,
+		},
+	})
+}
+
+func (u *UserController) DeleteAccount(c *gin.Context) {
+	id := c.Param("id")
+
+	errDB := u.DB.Delete(&models.User{}, id).Error
+	if errDB != nil {
+		c.JSON(http.StatusInternalServerError, models.Response{
+			StatusCode: http.StatusInternalServerError,
+			Message:    "Something went wrong",
+			Data:       nil,
+		})
+		fmt.Println("Error: errDB", errDB.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, models.Response{
+		StatusCode: http.StatusOK,
+		Message:    "Account deleted successfully",
+		Data:       nil,
+	})
+}
+
+func (u *UserController) GetEmployee(c *gin.Context) {
+
+	users := []models.User{}
+
+	errDB := u.DB.Select("name, email, role").Where("role = ?", "Employee").Find(&users).Error
+	if errDB != nil {
+		c.JSON(http.StatusInternalServerError, models.Response{
+			StatusCode: http.StatusInternalServerError,
+			Message:    "Something went wrong",
+			Data:       nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, models.Response{
+		StatusCode: http.StatusOK,
+		Message:    "Success get employee",
+		Data:       users,
+	})
+}
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"tusk/config"
+	"tusk/controllers"
+	"tusk/models"
+)
+
+func main() {
+	// Database
+	db := config.DatabaseConnection()
+	db.AutoMigrate(&models.User{}, &models.Task{})
+	config.CreateOwnerAccount(db)
+
+	// Controller
+	userController := controllers.UserController{DB: db}
+
+	// Router
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "Hello World")
+	})
+
+	router.POST("/users/login", userController.Login)
+	router.POST("/users", userController.CreateAccount)
+	router.DELETE("/users/:id", userController.DeleteAccount)
+	router.GET("/users/employee", userController.GetEmployee)
+	router.Static("/attachments", "./attachments")
+	// Start server
+	router.Run("192.168.110.216:8080")
+}
